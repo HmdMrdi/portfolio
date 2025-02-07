b60c8e4a7250c617ec7cd15998aed384626fcf21
Hamid Moradi <50118270+HmdMrdi@users.noreply.github.com>
2025-02-07 15:00:27 +0000

    Update script.js
    Automatic night mode

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+// Function to check and apply night mode based on system preference
+function applySystemNightMode() {
+  const body = document.body;
+  
+  // Check if the user prefers dark mode
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    body.classList.add('night-mode');
+  }
+}
+
 function toggleNightMode() {
   const body = document.body;
   const toggleLink = document.getElementById("night-mode-toggle");
@@ -17,3 +27,5 @@ function toggleNightMode() {
     toggleLink.textContent = "Night Mode";
   }
 }
+
+window.addEventListener('load', applySystemNightMode);
